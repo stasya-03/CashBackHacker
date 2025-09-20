efed12486879dc50197c105578732a563023e590
Anastasia <nastya.il03@mail.ru>
2025-09-20 12:17:03 +0400

    add testng branch and tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,37 @@
+package ru.netology.service;
+
+
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldReturnAmountToBoundaryWhenAmountIsLessThanBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturnZeroWhenAmountIsBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturnAmountToNextBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 2501;
+        int actual = service.remain(amount);
+        int expected = 499;
+        assertEquals(actual, expected);
+    }
+
+}
